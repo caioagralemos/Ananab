a9e063c1b933248a88157200b2edf7a533cc245a
Caio <caioagralemos@live.com>
2024-06-26 02:06:23 -0300

    Initial Commit

---
+++ Ananab/AnanabApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnanabApp.swift
+//  Ananab
+//
+//  Created by Caio on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AnanabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
